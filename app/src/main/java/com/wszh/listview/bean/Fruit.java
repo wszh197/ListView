f955b410e43527c78d9af96c92786e2c08cd8678
weishanzhou <6401088@qq.com>
2016-07-24 15:41:59 +0800

    second

--- app/src/main/java/com/wszh/listview/bean/Fruit.java
+++ app/src/main/java/com/wszh/listview/bean/Fruit.java
@@ -4,5 +4,19 @@ package com.wszh.listview.bean;
  * Created by lsx on 2016/7/24.
  */
 public class Fruit {
+    private String mName;
+    private int mImageId;
 
+    public Fruit(String mName, int mImageId) {
+        this.mName = mName;
+        this.mImageId = mImageId;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmImageId() {
+        return mImageId;
+    }
 }
